1c9084e953e67c1101bf8472ed7b4ad141de7361
bosshii <amackjp@gmail.com>
2023-01-16 19:52:22 +0900

    20230116-2 disable elements only in toppage

--- src/layouts/MainTop.vue
+++ src/layouts/MainTop.vue
@@ -2,7 +2,7 @@
   <v-navigation-drawer app v-model="drawer" color="blue accent-2" >
     <v-list>
       <v-list-subheader style="color:white">メインメニュー</v-list-subheader>
-      <v-list-item rounded="xl" value="1" to="/home">
+      <v-list-item rounded="xl" value="1" to="/home" v-if="!toppage">
         <template v-slot:prepend><v-icon color="white">mdi-home</v-icon></template>
         <v-list-item-title>ホーム画面</v-list-item-title>
       </v-list-item>
@@ -22,8 +22,8 @@
         <template v-slot:prepend><v-icon>mdi-rabbit</v-icon></template>
         <v-list-item-title>公式ホームページへ</v-list-item-title>
       </v-list-item>
-      <v-divider></v-divider>
-      <v-list-item rounded="xl" value="6" to="/">
+      <v-divider v-if="!toppage"></v-divider>
+      <v-list-item rounded="xl" value="6" to="/" v-if="!toppage">
         <template v-slot:prepend><v-icon>mdi-arrow-u-left-top</v-icon></template>
         <v-list-item-title>ログイン画面に戻る</v-list-item-title>
       </v-list-item>
@@ -35,7 +35,7 @@
       <span class="text-uppercase black--text ml-2 mt-1 mr-3">
         <router-link :to=" { name: 'home' } "><img src="/logo.svg" style="width:35vw; max-width:300px" alt="KUSS-WebMap" class="mt-3"></router-link>
       </span>
-      <v-menu transition="slide-y-transition">
+      <v-menu transition="slide-y-transition" v-if="!toppage">
         <template v-slot:activator="{ props }">
           <v-btn color="primary" v-bind="props" prepend-icon="mdi-chevron-down" variant="outlined">モード</v-btn>
         </template>
@@ -51,7 +51,7 @@
         </v-list>
       </v-menu>
       <v-spacer></v-spacer>
-      <v-dialog v-model="legend" width="500px">
+      <v-dialog v-model="legend" width="500px" v-if="!toppage">
         <template v-slot:activator="{ props }">
           <v-btn v-bind="props" prepend-icon="mdi-help" rounded="pill" class="legendbtn bg-primary">凡例</v-btn>
         </template>
@@ -69,12 +69,13 @@
   </v-app-bar>
 </template>
 <script>
-  export default {
+export default {
+    props: ['toppage'],
     data: () => ({
-      drawer: false,
-      legend: false,
-    }),
-  }
+          drawer: false,
+          legend: false,
+        }),
+}
 </script>
 <style>
 .legendbtn {
--- src/views/TopPage.vue
+++ src/views/TopPage.vue
@@ -1,32 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer app v-model="drawer" color="blue accent-2" >
-      <v-list>
-        <v-list-subheader style="color:white">メニュー</v-list-subheader>
-        <v-list-item style="color:white" rounded="xl" value="1" to="/about">
-          <template v-slot:prepend><v-icon color="white">mdi-information-outline</v-icon></template>
-          <v-list-item-title>このサイトについて</v-list-item-title>
-        </v-list-item>
-        <v-list-item style="color:white" rounded="xl" value="1" to="/credits">
-          <template v-slot:prepend><v-icon color="white">mdi-wrench</v-icon></template>
-          <v-list-item-title>クレジット</v-list-item-title>
-        </v-list-item>
-        <v-list-item style="color:white" rounded="xl" value="1" href="#">
-          <template v-slot:prepend><v-icon color="white">mdi-open-in-new</v-icon></template>
-          <v-list-item-title>学校ホームページへ</v-list-item-title>
-        </v-list-item>
-        <v-list-item style="color:white" rounded="xl" value="1" href="#">
-          <template v-slot:prepend><v-icon color="white">mdi-rabbit</v-icon></template>
-          <v-list-item-title>公式ホームページへ</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar class="gray lighten-5" app dense elevate-on-scroll>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title class="text-uppercase mt-1">
-        <span><img src="/logo.svg" alt="KUSS-WebMap" class="mt-3"></span>
-      </v-toolbar-title>
-    </v-app-bar>
+    <MainTop toppage="true"/>
     <v-main class="mb-4" style="background-color:#f1f3f5">
       <v-container class="my-3">
       <v-alert type="info" variant="outlined">現在準備中です。</v-alert>
@@ -52,30 +26,14 @@
   </v-app>
 </template>
 <script>
-export default {
-  name: "top",
-  data(){
-    return{
-          drawer: false,
-    }
+  export default {
+    data: () => ({
+      drawer: false,
+      legend: false,
+    }),
   }
-}
 </script>
-
-<style>
-a {
-  text-decoration:none;
-}
-.text-h3 {
-  border-bottom: solid 3px #E91E63;
-}
-.text-h4 {
-  padding: 0rem 0rem 0rem 2rem;
-  border-left: 4px solid #EC407A;
-}
-.v-toolbar__extension {
-  display: grid;
-  justify-content: center;
-  width: 100%;
-}
-</style>
+<script setup>
+  import MainTop from '@/layouts/MainTop.vue'
+  import CommonStyles from '@/layouts/CommonStyles.vue'
+</script>
