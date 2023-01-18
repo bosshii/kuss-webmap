3acf7acbc91c0836f300ad1ec1f5e29218c4d81a
bosshii <amackjp@gmail.com>
2023-01-18 23:13:21 +0900

    20230118-3 move legend to mapmenu

--- src/layouts/MainTop.vue
+++ src/layouts/MainTop.vue
@@ -33,13 +33,14 @@
       </v-list-item>
     </v-list>
   </v-navigation-drawer>
-  <v-app-bar app>
+  <v-app-bar app flat>
     <v-container class="fill-height d-flex align-center">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <span class="text-uppercase black--text ml-2 mt-1 mr-3">
         <router-link :to=" { name: 'home' } "><img src="/logo.svg" style="width:35vw; max-width:300px" alt="KUSS-WebMap" class="mt-3" v-if="!toppage"></router-link>
         <img src="/logo.svg" style="width:35vw; max-width:300px" alt="KUSS-WebMap" class="mt-3" v-if="toppage">
       </span>
+      <v-spacer></v-spacer>
       <v-menu transition="slide-y-transition" v-if="!toppage">
         <template v-slot:activator="{ props }">
           <v-btn color="primary" v-bind="props" prepend-icon="mdi-chevron-down" variant="outlined">モード</v-btn>
@@ -55,21 +56,6 @@
           </v-list-item>
         </v-list>
       </v-menu>
-      <v-spacer></v-spacer>
-      <v-dialog v-model="legend" width="500px" v-if="!toppage">
-        <template v-slot:activator="{ props }">
-          <v-btn v-bind="props" prepend-icon="mdi-information-outline" rounded="pill" class="legendbtn bg-primary">凡例</v-btn>
-        </template>
-        <v-card>
-          <v-card-title class="d-flex justify-center mt-2 mb-n5 font-weight-bold">凡例</v-card-title>
-          <v-card-text>
-            <v-img src="@/assets/map/legend.png"></v-img>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="primary" block @click="legend = false">閉じる</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
     </v-container>
   </v-app-bar>
 </template>
@@ -78,12 +64,6 @@ export default {
     props: ['toppage'],
     data: () => ({
           drawer: false,
-          legend: false,
         }),
 }
 </script>
-<style>
-.legendbtn {
-  justify-content: right;
-}
-</style>
